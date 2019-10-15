229cef6a9a062cd2c3a935f145f7f3692c085b91
anthonykao10 <anthonykao10@gmail.com>
2019-10-15 15:57:01 +0000

    add one to many association for store to employee. Add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "first1", last_name: "last1", hourly_rate: 10)
+@store1.employees.create(first_name: "first2", last_name: "last2", hourly_rate: 20)
+@store1.employees.create(first_name: "first3", last_name: "last3", hourly_rate: 30)
+
+@store2.employees.create(first_name: "first10", last_name: "last10", hourly_rate: 100)
+@store2.employees.create(first_name: "first11", last_name: "last11", hourly_rate: 110)
+@store2.employees.create(first_name: "first12", last_name: "last12", hourly_rate: 120)
